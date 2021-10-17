44be35a74262bb60c4d5e5f5bc41d643d9597c4e
Franco7960 <contreras.ventura.francisco@gmail.com>
2021-10-16 19:35:54 -0500

    Cambios Programa

--- java/AlgoritmoDJ.java
+++ java/AlgoritmoDJ.java
@@ -17,9 +17,8 @@ class AlgoritmoDJ{
      // AQUI EMPIEZO A MODIFICAR PROGRAMA
 
     System.out.print("Hola, es mi prueba con GITHUB");
-    System.out.print("cambios para realizar programas DJ");
-    System.out.print("Intentar mostrar cambios en GITHUB");
-    
+    System.out.print("Es la primera vez que utilizo esta aplicacion profe");
+    System.out.print("pero esta facil");
     } 
     public static void main(String[] args) {
         AlgoritmoDJ algoritmoDJ = new AlgoritmoDJ();
