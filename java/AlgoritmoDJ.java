b329090c2889aa72f9f84de2bc67d403bab622ff
Franco7960 <contreras.ventura.francisco@gmail.com>
2021-10-18 16:41:13 -0500

    Update: cambios finales

--- java/AlgoritmoDJ.java
+++ java/AlgoritmoDJ.java
@@ -15,64 +15,58 @@ class AlgoritmoDJ{
      * En este metodo se incia el algoritmo
      */
      // AQUI EMPIEZO A MODIFICAR PROGRAMA
-
-    import java.util.ArrayList;
-
-public class ADijkstra {
-    Status status;
-    
-    private ArrayList<Vertex> vertex;
-    private ArrayList<Integer> costos;
-    
-    public ADijkstra(){
-        this.status = new Status();
-
-        this.vertex = new ArrayList<Vertex>();
-    }
-    public boolean addVertex(String values){
-        boolean statusAdd = false;
-        if( this.status.getStatusAlgo() ){
-            Vertex verte = this.setVertex(values);            
-
-            this.status.startAlgo();
-            this.status.setSizeNodes( verte.getSize() );
-            this.vertex.add(verte);
-            statusAdd = true;
-
-        }else{
-            Vertex verte = this.setVertex(values);
-
-            if(this.status.getSizeNodes() == verte.getSize()){
-                this.vertex.add(verte);
-                statusAdd = true;
-            }else{
-                statusAdd = false;
-            }
-        }
-        return statusAdd;
-    }
-    private Vertex setVertex(String values){
-        Vertex verte = new Vertex();
-        for ( String cost : values.split(",") ) {
-            verte.addVertex( Integer.parseInt(cost) );
-        }
-        return verte;
-    }
-    public String getMessageInit(){
-        return this.status.startMessage();
-    }
-    public String getVertex(){
-        String adjacencyMatrix = "\n";
-
-        for (Vertex verte: this.vertex) {
-            for(int i = 0; i < this.status.getSizeNodes();i++){               
-                adjacencyMatrix += String.format("%10s",Integer.toString( verte.getVertex().get(i) )) ;
-            }
-            adjacencyMatrix += "\n";
-        }
-        return adjacencyMatrix;
-    }
+	public  int distancia[] = new int[10];
+	public  int costo[][] = new int[10][10]; 
+	public void calc(int n,int s){
+	  int flag[] = new int[n+1];
+	  int i,minpos=1,k,c,minimum;
+	  for(i=1;i<=n;i++)
+	  {  
+		flag[i]=0; 
+		this.distancia[i]=this.costo[s][i]; 
+	  }
+	  c=2;
+	  while(c<=n)
+	  {
+		minimum=99;
+		for(k=1;k<=n;k++){ 
+			if(this.distancia[k]<minimum && flag[k]!=1){
+			minimum=this.distancia[i];
+			minpos=k;
+		   }
+		} 
+		  flag[minpos]=1;
+		  c++;
+			for(k=1;k<=n;k++){
+				if(this.distancia[minpos]+this.costo[minpos][k] <  this.distancia[k] && flag[k]!=1 )
+					this.distancia[k]=this.distancia[minpos]+this.costo[minpos][k];
+			}   
+		}
+	}
+	
+ public static void main(String args[]){
+	int nodos,origen,i,j;
+	Scanner in = new Scanner(System.in);
+	System.out.print("Ingresa el numero de nodos: ");
+	nodos = in.nextInt();
+	Dijkstra d = new Dijkstra();
+	System.out.println("Ingresa distancias dejando espacios: ");
+        for(i=1;i<=nodos;i++){
+          for(j=1;j<=nodos;j++){
+            d.costo[i][j]=in.nextInt();
+            if(d.costo[i][j]==0)
+              d.costo[i][j]=999;
+			}
+		}
+  System.out.println("Ingresa nodo de origen : ");
+  origen=in.nextInt();
+  d.calc(nodos,origen);
+        for(i=1;i<=nodos;i++)
+          if(i!=origen)
+  System.out.println(" Origen :"+origen+"\t Destino :"+i+"\t Minimo :"+d.distancia[i]+"\t");
+	} 
 }
+    
     } 
     public static void main(String[] args) {
         AlgoritmoDJ algoritmoDJ = new AlgoritmoDJ();
