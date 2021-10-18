7c5cc9f7bee011bf7b496a34427c58fc308efa77
Franco7960 <contreras.ventura.francisco@gmail.com>
2021-10-18 13:27:54 -0500

    update: ADijkstra

--- java/AlgoritmoDJ.java
+++ java/AlgoritmoDJ.java
@@ -16,10 +16,63 @@ class AlgoritmoDJ{
      */
      // AQUI EMPIEZO A MODIFICAR PROGRAMA
 
-    System.out.print("Hola, es mi prueba con GITHUB");
-    System.out.print("Es la primera vez que utilizo esta aplicacion profe");
-    System.out.print("pero esta facil");
-    System.out.println("cambios");
+    import java.util.ArrayList;
+
+public class ADijkstra {
+    Status status;
+    
+    private ArrayList<Vertex> vertex;
+    private ArrayList<Integer> costos;
+    
+    public ADijkstra(){
+        this.status = new Status();
+
+        this.vertex = new ArrayList<Vertex>();
+    }
+    public boolean addVertex(String values){
+        boolean statusAdd = false;
+        if( this.status.getStatusAlgo() ){
+            Vertex verte = this.setVertex(values);            
+
+            this.status.startAlgo();
+            this.status.setSizeNodes( verte.getSize() );
+            this.vertex.add(verte);
+            statusAdd = true;
+
+        }else{
+            Vertex verte = this.setVertex(values);
+
+            if(this.status.getSizeNodes() == verte.getSize()){
+                this.vertex.add(verte);
+                statusAdd = true;
+            }else{
+                statusAdd = false;
+            }
+        }
+        return statusAdd;
+    }
+    private Vertex setVertex(String values){
+        Vertex verte = new Vertex();
+        for ( String cost : values.split(",") ) {
+            verte.addVertex( Integer.parseInt(cost) );
+        }
+        return verte;
+    }
+    public String getMessageInit(){
+        return this.status.startMessage();
+    }
+    public String getVertex(){
+        String adjacencyMatrix = "\n";
+
+        for (Vertex verte: this.vertex) {
+            for(int i = 0; i < this.status.getSizeNodes();i++){               
+                adjacencyMatrix += String.format("%10s",Integer.toString( verte.getVertex().get(i) )) ;
+            }
+            adjacencyMatrix += "\n";
+        }
+        return adjacencyMatrix;
+    }
+}
     } 
     public static void main(String[] args) {
         AlgoritmoDJ algoritmoDJ = new AlgoritmoDJ();
